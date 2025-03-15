90b66df9654322bfc5505907fb7a9a59bf94f99e
bandit7311 <40744107+bandit7311@users.noreply.github.com>
2025-03-15 13:35:04 +0100

    Update LD2450.cpp

--- components/LD2450/LD2450.cpp
+++ components/LD2450/LD2450.cpp
@@ -320,6 +320,10 @@ namespace esphome::ld2450
 
             // Filter targets further than max detection distance and max angle
             float angle = -(atan2(y, x) * (180 / M_PI) - 90);
+            
+            // temp update
+            targets_[i]->update_values(x, y, speed, distance_resolution);
+           
             if ((y <= max_detection_distance_ || (targets_[i]->is_present() && y <= max_detection_distance_ + max_distance_margin_)) &&
                 (angle <= max_detection_tilt_angle_ || (targets_[i]->is_present() && angle <= max_detection_tilt_angle_ + tilt_angle_margin_)) &&
                 (angle >= min_detection_tilt_angle_ || (targets_[i]->is_present() && angle >= min_detection_tilt_angle_ - tilt_angle_margin_)))
